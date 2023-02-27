c9c10914d4299cb1f229b001a1206b1408b5d68f
Chris Barnett <cbarnett2813@gmail.com>
2023-02-27 09:45:50 -0600

    Created initial components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import Posts from "./components/Posts/Posts";
+
+function App() {
+  return (
+    <div className="App">
+    <Posts></Posts>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,16 @@
+
+import React, {Component} from 'react';
+import "./Posts.scss"
+class Posts extends Component {
+    render() {
+        return (
+            <div className="Posts">
+                Our post compotent
+            </div>
+        );
+    }
+}
+
+Posts.propTypes = {};
+
+export default Posts;
